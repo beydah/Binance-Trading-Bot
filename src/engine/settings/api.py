fc782977daf59701a1dd5a7de4e480399f16106b
Beydah <beydahsaglam@hotmail.com>
2024-03-25 16:51:40 +0300

    Version 1.0.0

---
+++ src/engine/settings/api.py
@@ -0,0 +1,13 @@
+# ----------------------------------------------------------------
+# Added Links
+from src.engine.settings import library as LIB
+# ----------------------------------------------------------------
+# Your API Keys
+Binance_Key = ""
+Binance_Secret = ""
+Telegram_Bot_Token = ""
+Telegram_User_ID = ""
+# ----------------------------------------------------------------
+# Your Binance Bots
+Binance = LIB.BINANCE(Binance_Key, Binance_Secret)
+# ----------------------------------------------------------------
